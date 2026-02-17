e14c24deae878e9379aefab2309c57d3919b6121
rizzz.dev <rizqonmaulana05@gmail.com>
2026-02-17 19:17:18 +0700

    fix: fix error in prod env part 2

--- src/app/middlewares/image-uploader.middleware.ts
+++ src/app/middlewares/image-uploader.middleware.ts
@@ -1,31 +1,12 @@
 import type { NextFunction, Request, Response } from "express";
-import multer, { type FileFilterCallback } from "multer";
+import multer from "multer";
 import { BadRequestError } from "../../shared/errors/index.js";
-import { imageUploaderApi } from "../../shared/utils/image-uploader-api.util.js";
-import fs from "fs";
-import axios from "axios";
-import { createClient } from "@supabase/supabase-js";
 import { localDateNow } from "../../shared/utils/date.util.js";
 import { supabaseClient } from "../database/supabase/index.js";
 
-// const storage = multer.diskStorage({
-//   destination: (request, file, callback) => {
-//     callback(null, "./public");
-//   },
-// });
-
 const MB_IN_BYTES = 1024 * 1024;
 
-// export const upload = multer({
-//   storage,
-// });
-
 const uploadImage = () => {
-  // const isPathExist = fs.existsSync(process.cwd() + path);
-  // if (!isPathExist) {
-  //   fs.mkdirSync(process.cwd() + path);
-  // }
-
   const storage = multer.memoryStorage();
 
   return multer({
